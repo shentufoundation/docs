056e57668b9bfa6769257164d424458b13a78b97
skyargos <binskyargos@gmail.com>
2024-12-18 15:06:21 +0800

    update config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -39,8 +39,8 @@ const config: Config = {
           sidebarPath: './sidebars.ts',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-              'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          // editUrl:
+          //     'https://github.com/shentufoundation/docs/blob/main/',
         },
         // blog: {
         //   showReadingTime: true,
@@ -51,7 +51,7 @@ const config: Config = {
         //   // Please change this to your repo.
         //   // Remove this to remove the "edit this page" links.
         //   editUrl:
-        //       'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        //       'https://github.com/shentufoundation/docs/blob/main',
         //   // Useful options to enforce blogging best practices
         //   onInlineTags: 'warn',
         //   onInlineAuthors: 'warn',
@@ -66,7 +66,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    // image: 'img/docusaurus-social-card.jpg',
     navbar: {
       title: 'Shentu Docs',
       logo: {
@@ -127,7 +127,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Shentu Foundation.`,
     },
     prism: {
       theme: prismThemes.github,
